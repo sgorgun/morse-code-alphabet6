8865ca607c95931833bab4364ca6d1437b160e7b
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-08 09:17:41 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ MorseCodeAlphabet/UsingIf.cs
@@ -0,0 +1,16 @@
+﻿namespace MorseCodeAlphabet
+{
+    public static class UsingIf
+    {
+        public static string GetMorseCode(char c)
+        {
+            // TODO #1. Implement the method using "if" statement. The method should return a Morse code sequence using "." symbol as dot and "-" symbol as dash.
+            if (c == 'A' || c == 'a')
+            {
+                return ".-";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ MorseCodeAlphabet/UsingSwitch.cs
@@ -0,0 +1,19 @@
+﻿namespace MorseCodeAlphabet
+{
+    public static class UsingSwitch
+    {
+        public static byte GetMorseCode(char c)
+        {
+            // TODO #2. Implement the method using "switch/case" statements. The number should be represented the code as a binary literal.
+            switch (c)
+            {
+                case 'A':
+                case 'a':
+                    return 0b0011_0001;
+
+                default:
+                    return 0b0000_0000;
+            }
+        }
+    }
+}
